fe16194d3e50bdb647b0045e486622d5720e6465
YitzakEmileZam <codemanhh78@gmail.com>
2025-04-18 14:57:36 -0500

    Agregando vista registrarse

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Jham.Models;
+
+namespace Jham.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Registrarse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrarse(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aquí guardarías el usuario en la BD o lógica correspondiente
+                return RedirectToAction("RegistroExitoso");
+            }
+
+            return View(usuario);
+        }
+
+        public IActionResult RegistroExitoso()
+        {
+            return View();
+        }
+    }
+}
