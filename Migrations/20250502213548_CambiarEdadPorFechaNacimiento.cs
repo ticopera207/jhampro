468e2bd0c22049f747152feb885a4d8e2194d87d
ticopera207 <ticopera23@hotmail.com>
2025-05-02 16:42:29 -0500

    comintt

---
+++ Migrations/20250502213548_CambiarEdadPorFechaNacimiento.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jham.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiarEdadPorFechaNacimiento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Edad",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Dni",
+                table: "Usuarios",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(8)",
+                oldMaxLength: 8);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Usuarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Dni",
+                table: "Usuarios",
+                type: "character varying(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Edad",
+                table: "Usuarios",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
