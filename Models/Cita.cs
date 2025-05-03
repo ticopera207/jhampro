e503217791579acb717699168341de56286cae69
ticopera207 <ticopera23@hotmail.com>
2025-05-02 20:00:41 -0500

    termiando

--- Models/Cita.cs
+++ Models/Cita.cs
@@ -8,18 +8,29 @@ using System.ComponentModel.DataAnnotations.Schema; //Necesario para el uso de [
 namespace Jham.Models
 
 {
-    public class Cita
-    {
+  public class Cita
+{
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+
     public string EstadoCita { get; set; }
     public DateTime Fecha { get; set; }
     public TimeSpan Hora { get; set; }
 
+    [ForeignKey("Usuario")]
     public int UsuarioId { get; set; }
-    public Usuario Usuario { get; set; }
+    public Usuario Usuario { get; set; } // Por ejemplo: cliente
 
-    public Caso Caso { get; set; }
-    }
+    [ForeignKey("Usuario2")]
+    public int UsuarioId2 { get; set; }
+    public Usuario Usuario2 { get; set; } // Por ejemplo: quien atiende la cita
+
+    [ForeignKey("Abogado")]
+    public int AbogadoId { get; set; }
+    public Abogado Abogado { get; set; }
+
+    public Caso Caso { get; set; } // Si aquí también hay relación, considera añadir ForeignKey
+}
+
 }
