8e2a81e066d580840d611314bb2eacca4c1a6020
Mathías Cueto <mathyascb@hotmail.com>
2025-05-18 09:40:02 -0500

    Actualización Post-Migración

--- Migrations/20250517072114_CreateUsuarioServicioRelation.Designer.cs
+++ Migrations/20250517072114_CreateUsuarioServicioRelation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Jham.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250517072114_CreateUsuarioServicioRelation")]
+    partial class CreateUsuarioServicioRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -33,7 +36,7 @@ namespace Jham.Migrations
 
                     b.HasIndex("ServicioId");
 
-                    b.ToTable("abogado_servicio");
+                    b.ToTable("AbogadoServicio");
                 });
 
             modelBuilder.Entity("jhampro.Models.Servicio", b =>
