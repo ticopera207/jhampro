edba577498ed89e553a9505c63992824959ea578
Mathías Cueto <mathyascb@hotmail.com>
2025-05-27 02:34:56 -0500

    Actualización Post-Migración Pago

---
+++ Migrations/20250527073057_CreatePago.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Jham.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePago : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "pago",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FechaPago = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Monto = table.Column<decimal>(type: "numeric", nullable: false),
+                    Metodo = table.Column<string>(type: "text", nullable: false),
+                    ServicioId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pago", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_pago_servicio_ServicioId",
+                        column: x => x.ServicioId,
+                        principalTable: "servicio",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pago_ServicioId",
+                table: "pago",
+                column: "ServicioId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pago");
+        }
+    }
+}
