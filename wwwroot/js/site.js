2bf7037e5972a74722345d89823a6b7c77975794
AaronPalomino2002 <palominofalconaaron@gmail.com>
2025-05-31 13:22:08 -0500

    cambios en diseño

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -16,3 +16,22 @@ window.onclick = function (event) {
     popup.style.display = "none";
   }
 };
+
+const fadeInElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+fadeInElements.forEach((el) => observer.observe(el));
