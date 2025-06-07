6d7e6bd6b3d876b299b35333a9d34b8643c1ecd7
AaronPalomino2002 <palominofalconaaron@gmail.com>
2025-06-07 07:56:35 -0500

    nueva cista de perfil de abogado

---
+++ Controllers/AbogadoController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace jhampro.Controllers
+{
+    public class AbogadoController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AbogadoController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Abogado()
+        {
+            // Validar si el usuario ha iniciado sesión y es Administrador
+            var tipoUsuario = HttpContext.Session.GetString("TipoUsuario");
+
+            if (string.IsNullOrEmpty(tipoUsuario) || tipoUsuario != "Abogado")
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            // Puedes enviar datos a la vista con ViewBag o un modelo
+            ViewBag.UsuarioNombre = HttpContext.Session.GetString("UsuarioNombre");
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
