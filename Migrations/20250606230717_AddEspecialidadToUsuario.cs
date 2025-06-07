f77368ecefcc3d8ac82678a79d660928cdc93b05
ticopera207 <ticopera23@hotmail.com>
2025-06-06 19:05:59 -0500

    terminadoBD

---
+++ Migrations/20250606230717_AddEspecialidadToUsuario.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jham.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEspecialidadToUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Especialidad",
+                table: "usuario",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Especialidad",
+                table: "usuario");
+        }
+    }
+}
