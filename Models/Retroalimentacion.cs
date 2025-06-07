182fd0bf221a9b0cffadd017716ed4c658d94d51
Mathías Cueto <mathyascb@hotmail.com>
2025-06-06 23:39:06 -0500

    ActualizaciónBDv2

---
+++ Models/Retroalimentacion.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jhampro.Models
+{
+    [Table("retroalimentacion")]
+    public class Retroalimentacion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } // Clave primaria
+        public string Comentario { get; set; }
+        public int Calificacion { get; set; }
+        public DateTime Fecha { get; set; }
+
+        // Relación uno a uno (Retroalimentación a Servicio)
+        public int ServicioId { get; set; }
+        public Servicio Servicio { get; set; }
+    }
+}
