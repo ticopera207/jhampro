1f9dea69ebf52c161b74b641b5274129fea9dc3b
Mathías Cueto <mathyascb@hotmail.com>
2025-06-11 00:54:59 -0500

    Retroalimentación integración

---
+++ Migrations/20250607095619_CreateRetroalimentacionExtra.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jham.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRetroalimentacionExtra : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Publico",
+                table: "retroalimentacion",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Publico",
+                table: "retroalimentacion");
+        }
+    }
+}
